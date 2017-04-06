b7f568d65c69791cd59daaf6b34b1050585d372b
Bishwo Gurung <bishwo.gurung3@gmail.com>
2017-04-05 23:02:14 -0400

    Create a guest user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import AnonymousUser
 from django.db import models
 
-# Create your models here.
+class Guest(models.Model):
+    user = AnonymousUser()
